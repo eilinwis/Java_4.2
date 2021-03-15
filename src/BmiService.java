1d3eaf786f44668edb6738e822e74fb9a3b04909
Roman Kudryavtsev <TTe4kiH98@gmail.com>
2021-03-15 13:20:05 +0300

    Second commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,5 @@
 public class BmiService {
     public double calculate (double weight, double height ) {
-      //  float height;
-        //float weight;
         double bmi = weight / (height * height);
 
         return bmi;
